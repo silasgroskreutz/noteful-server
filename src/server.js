a8c013a0f5e3a66852d51d0aa23f35a0f147827d
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2019-11-18 14:30:28 -0600

    checkpoint 19 completed

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
+const knex = require('knex');
 const app = require('./app')
 
 const { PORT } = require('./config')
+
+const db = knex({
+  client:'pg',
+  connection: process.env.DATABASE_URL,
+
+});
+
+app.set('db', db);
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
